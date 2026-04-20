334d8ee4120a417384164d0d6fb104562b139fc1
DrakeSeteraO <oakleypaws@gmail.com>
2026-04-20 16:45:22 -0400

    Made fake data

---
+++ select_data.py
@@ -0,0 +1,15 @@
+FIRST_NAMES = [""]
+
+LAST_NAMES = [""]
+
+BANK_NAMES = [""]
+
+STATES = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI']
+
+CARD_TYPES = ['Visa', 'Mastercard']
+
+PLATFORMS = ['PC', 'XBOX', 'Play Station', 'Nintendo', 'Mobile']
+
+GENRES = ['Horror', 'Sandbox', 'Creative', 'Adventure', 'Magic', 'War', 'FPS', 'Puzzle', 'Multiplayer', 'RPG']
+
+NOUNS = ['Dog', 'Cat', 'Cow', 'Paper', 'Sand', 'Ocean', 'Water', 'Mountain', 'Forest', 'Woods', 'Dessert', 'City', 'Party', 'Lion', 'Tiger', 'Mouse', 'Llama', 'Peak', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
