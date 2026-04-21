9b7212235736344fd1b367600c2019c9e384ffa9
DrakeSeteraO <oakleypaws@gmail.com>
2026-04-20 21:52:48 -0400

    Added more possible fake data

--- select_data.py
+++ select_data.py
@@ -1,15 +1,17 @@
-FIRST_NAMES = [""]
+FIRST_NAMES = ['Kurt', 'Jeffrey', 'Steven', 'Ronald', 'Richard', 'Philip', 'Timothy', 'David', 'Douglas', 'Matthew','Kathleen', 'Suzanne', 'Gretchen', 'Lynda', 'Susan', 'Pamela', 'Sherri', 'John', 'James']
 
-LAST_NAMES = [""]
+LAST_NAMES = ['Smith', 'Johnson', 'Williams', 'Brown', 'Garcia', 'Martinez', 'Davis', 'Jones', 'Miller', 'Wilson', 'Thomas', 'Taylor', 'Lee', 'White', 'Young', 'Green']
 
-BANK_NAMES = [""]
+BANK_NAMES = ['Chase Bank', 'Truist', 'Fifth Third Bank', 'Bank of America', 'TD Bank', 'Citizens Financial Group', 'BMO Bank', 'Huntington Bancshares', 'KeyBank', 'M&T Bank']
 
 STATES = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI']
 
-CARD_TYPES = ['Visa', 'Mastercard']
+CARD_TYPES = ['Visa', 'Mastercard', 'Discover']
 
-PLATFORMS = ['PC', 'XBOX', 'Play Station', 'Nintendo', 'Mobile']
+PLATFORMS = ['PC', 'XBOX', 'XBOX 360', 'XBOX One', 'XBOX One S', 'XBOX One X', 'XBOX S', 'XBOX X', 'PS1', 'PS2', 'PS3', 'PS4', 'PS5', 'Wii', 'DS', '2DS', '3DS', 'Atari', 'PC']
 
 GENRES = ['Horror', 'Sandbox', 'Creative', 'Adventure', 'Magic', 'War', 'FPS', 'Puzzle', 'Multiplayer', 'RPG']
 
-NOUNS = ['Dog', 'Cat', 'Cow', 'Paper', 'Sand', 'Ocean', 'Water', 'Mountain', 'Forest', 'Woods', 'Dessert', 'City', 'Party', 'Lion', 'Tiger', 'Mouse', 'Llama', 'Peak', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+NOUNS = ['Dog', 'Cat', 'Cow', 'Paper', 'Sand', 'Ocean', 'Water', 'Mountain', 'Forest', 'Woods', 'Dessert', 'City', 'Party', 'Lion', 'Tiger', 'Mouse', 'Llama', 'Peak']
+
+ADJECTIVES = ['Small', 'Tall', 'Large', 'Tiny', 'Shiny', 'Dull', 'Rough', 'Smooth', 'Fast', 'Slow', 'Happy', 'Weird', 'Angry', 'Sad', 'Scared']
